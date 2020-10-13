ea56914d5a44a5f2279e32812c57cba302724dd5
Anuradha4dm <damithanuradha44@gmail.com>
2020-10-14 00:50:25 +0530

    First Commint with inital files

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -3,24 +3,10 @@ import { NotificationModel } from '../models/notification.module';
 export class NotificationService {
   private notifications: NotificationModel[] = [
     new NotificationModel(
-      'damith',
-      'nadeeka',
-      'Serious Problem 1',
-      'You are my hotti',
-      new Date().toUTCString()
-    ),
-    new NotificationModel(
-      'damith',
-      'nadeeka',
+      'user1',
+      'user2',
       'Serious Problem 2',
-      'Not Inesent ,Care About me,best person i have ever seen ',
-      new Date().toUTCString()
-    ),
-    new NotificationModel(
-      'damith',
-      'nadeeka',
-      'Serious Problem 2',
-      "I Can't live without you",
+      '',
       new Date().toUTCString()
     ),
   ];
