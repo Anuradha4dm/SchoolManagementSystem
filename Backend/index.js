db8e3216c912214405af40566a745af20a77e794
Anuradha4dm <damithanuradha44@gmail.com>
2020-10-22 00:53:31 +0530

    start with the backend

---
+++ Backend/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+//include the body to the parse
+const app = express();
+
+//these are the main routing modules
+const studentRoute = require('./routes/studentRouter');
+const sequelize = require('./util/databaseConnection');
+
+//resolving CORS errors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '* ');
+    res.setHeader('Access-Control-Allow-Method', 'GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+})
+
+
+
+//include the body parser 
+app.use(bodyParser.json());
+
+app.use(studentRoute);
+
+
+sequelize.sync({ force: true })
+    .then(result => {
+        console.log("Connection success")
+        app.listen(3000);
+    })
+    .catch(error => {
+        console.log('error')
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
