deab27202ac71e6e29fb55630560aa482508db97
Anuradha4dm <damithanuradha44@gmail.com>
2020-10-26 13:21:59 +0530

    login authentication success

---
+++ src/app/component/login/user-login.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class UserLogInService {
+  errorEmitEvent = new Subject<{ error: string }>();
+
+  userAuthData: {
+    _id: string;
+    authentication: boolean;
+    logInAs: string;
+    token: string;
+  };
+
+  constructor(private httpClient: HttpClient, private router: Router) {}
+
+  postLogInUser(formData: { _id: string; password: string }) {
+    this.httpClient
+      .post<{
+        _id: string;
+        authentication: boolean;
+        logInAs: string;
+        token: string;
+      }>('http://localhost:3000/auth/login', formData)
+
+      .subscribe(
+        (result) => {
+          this.userAuthData = result;
+          this.router.navigate['userprofile'];
+        },
+        ({ error }) => {
+          this.errorEmitEvent.next({ error: error.message });
+        },
+        () => {
+          console.log('complete');
+        }
+      );
+  }
+}
