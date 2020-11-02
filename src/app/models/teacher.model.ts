5e3e357f1e21dbe32b5c869fee45194e73589823
Madushan96 <52341473+Madushan96@users.noreply.github.com>
2020-11-03 02:20:37 +0530

    Reset branch

---
+++ src/app/models/teacher.model.ts
@@ -0,0 +1,17 @@
+export class Teacher{
+    id: string;
+    surname: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    userName: string;
+    imagePath?: string;
+    gender: string;
+    age: number;
+    role: string;
+    subjects?: string;
+    class: string;
+    leaveTaken?: number;
+    leaveLeft?: number;
+    qualification?: string; 
+}
