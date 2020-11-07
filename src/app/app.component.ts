21ea354f6970164502b8b5a13b7157aaf025045a
Anuradha4dm <damithanuradha44@gmail.com>
2020-11-07 13:37:35 +0530

    notification part is completed not realtime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WebSocketService } from './services/websocket.service';
 
 @Component({
@@ -6,10 +6,15 @@ import { WebSocketService } from './services/websocket.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'SchoolMangementSystem';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+  }
 }
