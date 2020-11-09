1984b56608ea7c60c9e77af8e540973cb0c41cd7
Anuradha4dm <damithanuradha44@gmail.com>
2020-11-09 22:11:19 +0530

    notificatin is complete

--- src/app/models/notification.modele.ts
+++ src/app/models/notification.modele.ts
@@ -2,7 +2,7 @@ export class NotificationModel {
   public notificationid: string;
   public from: string;
   public to: string;
-
+  public title: string;
   public description: string;
   public time: string;
   public publisher: string;
@@ -12,14 +12,14 @@ export class NotificationModel {
     publisher: string,
     from: string,
     to: string,
-
+    title: string,
     description: string,
     time: string
   ) {
     this.notificationid = notid;
     this.from = from;
     this.to = to;
-
+    this.title = title;
     this.description = description;
     this.time = time;
     this.publisher = publisher;
--- src/app/services/Notification-resolver.service.ts
+++ src/app/services/Notification-resolver.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { exhaustMap, map, take } from 'rxjs/operators';
+import { UserLogInService } from '../component/homepage/login/user-login.service';
 import { NotificationModel } from '../models/notification.modele';
 import { NotificationService } from './notification.service';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationResolver implements Resolve<NotificationModel[]> {
-  constructor(private notificationService: NotificationService) {}
+  constructor(
+    private notificationService: NotificationService,
+    private userLoginService: UserLogInService
+  ) {}
 
   resolve(
     route: ActivatedRouteSnapshot
@@ -16,23 +20,32 @@ export class NotificationResolver implements Resolve<NotificationModel[]> {
     | Promise<NotificationModel[]>
     | NotificationModel[] {
     var array: NotificationModel[] = [];
-    return this.notificationService.getNotifications().pipe(
-      map((data: any) => {
-        data.notificationArray.forEach((element) => {
-          array.push(
-            new NotificationModel(
-              element.notificationid,
-              element.publisher,
-              element.from,
-              element.to,
-              element.message,
-              element.createdAt
-            )
-          );
-        });
 
-        return array;
-      })
-    );
+    return this.userLoginService.userAuthData
+      .pipe(
+        take(1),
+        exhaustMap((user) => {
+          return this.notificationService.getNotifications(user.getUserId);
+        })
+      )
+      .pipe(
+        map((data: any) => {
+          data.notificationArray.forEach((element) => {
+            array.push(
+              new NotificationModel(
+                element.notificationid,
+                element.publisher,
+                element.from,
+                element.to,
+                element.title,
+                element.message,
+                element.createdAt
+              )
+            );
+          });
+
+          return array;
+        })
+      );
   }
 }
