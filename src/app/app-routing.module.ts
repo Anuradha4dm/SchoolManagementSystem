8c6258696393b9bc89bc7325006f0b327fa6b2ee
Madushan96 <52341473+Madushan96@users.noreply.github.com>
2020-11-22 06:40:39 +0530

    Create the reset page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomelayoutComponent } from './component/homepage/homelayout/homelayout.component';
+import { PasswordResetComponent } from './component/homepage/password-reset/password-reset.component';
+import { ResetPasswordComponent } from './component/reset-password/reset-password.component';
 import { SendEmilComponent } from './component/send-emil/send-emil.component';
 import { LayoutComponent } from './layout/layout.component';
 
@@ -15,6 +17,14 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [{ path: 'send-email', component: SendEmilComponent }],
   },
+  {
+    path: 'reset',
+    component: ResetPasswordComponent
+  },
+  {
+    path: 'new',
+    component: PasswordResetComponent
+  }
 
   // },
   // {
